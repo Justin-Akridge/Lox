bd6cd846b5faf8030537b41e729210ef524283dc
Justin Akridge <akridjus@gmail.com>
2024-01-25 20:43:29 -0500

    feat(initial commit) hello world

---
+++ Lox.java
@@ -0,0 +1,27 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+  public static void main(String[] args) throws IOException {
+    if (args.length > 1) {
+      System.out.println("Usage: jlox [script]");
+      System.exit(64); 
+    } else if (args.length == 1) {
+      runFile(args[0]);
+    } else {
+      runPrompt();
+    }
+  }
+}
+
+private static void runFile(String path) throws IOExpection {
+  byte[] bytes = Files.readAllBytes(Paths.get(path));
+  run(new String(bytes, Charset.defaultCharset()));
+}
---
+++ Makefile
@@ -0,0 +1,4 @@
+make:
+	javac Lox.java && java Lox
+clean:
+	clean
